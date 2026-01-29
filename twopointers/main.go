13d0465eb6d1e95b82797f9c980222d0304a9a51
keerthi.r <keerthi.r@thekaios.com>
2026-01-29 16:00:40 +0530

    feat:added a two pointer sums

---
+++ twopointers/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("inside main")
+
+	arr := []int{1, 2, 3, 4, 6}
+	target := 6
+	result := FindSum(arr, target)
+	fmt.Println(result)
+}
